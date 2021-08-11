fb504c8b25a4031ebede948bf7538bec5c779881
Lucas Aguiar <lucas.nune@outlook.com.br>
2021-08-11 18:40:11 -0300

    Laço de Repetição 2

---
+++ ProXGeneration/src/PacoteJava/Repeticao2.java
@@ -0,0 +1,32 @@
+package PacoteJava;
+
+import java.util.Scanner;
+
+public class Repeticao2 {
+	
+	public static void main(String[] args) 
+	{
+		Scanner entrada = new Scanner(System.in);
+		int idade;
+		
+		System.out.print("Entre com sua Idade: ");
+		idade = entrada.nextInt();
+		
+		
+		while(idade>=1)//Enquanto a condição for verdadeira - Executa o laço
+		{
+			System.out.printf("Sua Idade é %d ",idade);
+			if(idade>=18) 
+			{
+				System.out.println(" Você é de maior...");
+			}
+			else
+			{
+				System.out.println("Você é de menor...");
+			}
+			System.out.println("Entre com suia idade: ");
+			idade = entrada.nextInt();
+		}
+	}
+
+}
