e48ee0cdfb03bccfa99630468c44e18f864c5d25
Lucas Aguiar <lucas.nune@outlook.com.br>
2021-08-26 13:30:44 -0300

    Adicionando Conexão com o Banco de Dados | Repository

--- ProSpring/Atividades/atividadeTeleAulas/src/main/java/com/atividades/atividadeTeleAulas/controller/AtividadesC1.java
+++ ProSpring/Atividades/atividadeTeleAulas/src/main/java/com/atividades/atividadeTeleAulas/controller/AtividadesC1.java
@@ -3,6 +3,7 @@ package com.atividades.atividadeTeleAulas.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/atividade")
---
+++ ProSpring/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -0,0 +1,68 @@
+package org.generation.blogPessoal.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.persistence.Id; 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+
+@Entity // Essa Anotação ela indica que essa classe será uma entidade do JPA 
+@Table(name = "postagem") // Essa Anotação ela indica que essa entidade dentro do banco de dados ira ser tornar uma TABELA
+public class Postagem {
+	
+	//Declarando Váriaveis 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String titulo;
+	
+	@NotNull
+	@Size(min =5, max = 100)
+	private String texto; 
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis()); // java.sql captura a data exata do momento em que os dados foram inseridos
+	
+	
+	
+	
+	//Getters and Setters 
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	
+
+}
