22b5ade1b240f83fc601a4ad9f0fb52d24677534
Lucas Aguiar <lucas.nune@outlook.com.br>
2021-08-29 12:42:53 -0300

    Adicionando método PUT

--- ProSpring/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ ProSpring/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,13 +39,18 @@ public class PostagemController {
 	}			//map captura uma resposta positiva	//orElse nessa situação tem uma 
 													//condição de notFound caso não exista essa opção requisitada
 	
-	@GetMapping("/titulo/{titulo}")
+	@GetMapping("/titulo/{titulo}") // Pega uma informação
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
 	
-	@PostMapping
+	@PostMapping //Envia informações pro banco com POST
 	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	@PutMapping //Sobrescreve uma POST 
+	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
 	}
 }
