7e8bf37814974ed519111922a652fb87b43d7b14
Lucas Aguiar <lucas.nune@outlook.com.br>
2021-08-31 17:47:47 -0300

    Iniciando Projeto DrugStore

--- ProSpring/drugstore/src/main/java/br/com/Drugstore/drugstore/controller/CategoriaController.java
+++ ProSpring/drugstore/src/main/java/br/com/Drugstore/drugstore/controller/CategoriaController.java
@@ -1,5 +1,30 @@
 package br.com.Drugstore.drugstore.controller;
 
-public class CategoriaController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.Drugstore.drugstore.model.Categoria;
+import br.com.Drugstore.drugstore.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CategoriaController {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	
+	//Anotação para requisição 
+	@GetMapping
+	public ResponseEntity<List<Categoria>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
 }
