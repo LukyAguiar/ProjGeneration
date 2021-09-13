9b8d8e899c769f7cf9602e08e3f2565e719a1977
Lucas Aguiar <lucas.nune@outlook.com.br>
2021-09-13 17:27:51 -0300

    Test UsuarioController

--- ProSpring/blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ ProSpring/blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -1,14 +1,18 @@
 package org.generation.blogPessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.generation.blogPessoal.model.UserLogin;
 import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.UsuarioRepository;
 import org.generation.blogPessoal.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +27,24 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioService usuarioService;
 	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	//Método  Get buscando todas as informações
+	@GetMapping("/all")
+	public ResponseEntity<List<Usuario>> GetAll(){
+		return ResponseEntity.ok(usuarioRepository.findAll());
+	}
+	
+	//Método Get buscando pelo ID
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id){
+		return usuarioRepository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	// Método POST para logar
 	@PostMapping ("/logar")
 	public ResponseEntity <UserLogin> Authentication (@RequestBody Optional <UserLogin> user) {
 		return usuarioService.Logar(user)
@@ -30,11 +52,15 @@ public class UsuarioController {
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
 	
+	
+	//Método POST para Cadastrar
 	@PostMapping ("/cadastrar")
 	public ResponseEntity <Usuario> post(@RequestBody Usuario usuario) {
 		return usuarioService.CadastrarUsuario(usuario)
 				.map (resp -> ResponseEntity.status (HttpStatus.CREATED).body(resp))
 				.orElse (ResponseEntity.status(HttpStatus.CONFLICT).build());
 	}
+	
+	
 	
 }
