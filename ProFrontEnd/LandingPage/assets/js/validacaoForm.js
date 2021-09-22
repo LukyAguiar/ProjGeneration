b3986b958c7210171543f1b1ef7db56f00e2400c
Lucas Aguiar <lucas.nune@outlook.com.br>
2021-09-22 12:38:00 -0300

    Valição Javascript

---
+++ ProFrontEnd/LandingPage/assets/js/validacaoForm.js
@@ -0,0 +1,6 @@
+alert("Seja bem vindo ♥")
+
+/*Buscando o valor no input*/
+let nome = document.querySelector('input#nome')
+let email = document.querySelector('input#email')
+let assunto = document.querySelector('#assunto')
