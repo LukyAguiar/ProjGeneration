4f4880b68b34d96fd14ff2df81cee836fd1c6581
Lucas Aguiar <lucas.nune@outlook.com.br>
2021-09-29 15:09:49 -0300

    Model Usuario

---
+++ ProFrontEnd/Angular/Projeto/blogPessoal/src/app/model/Usuario.ts
@@ -0,0 +1,11 @@
+import { StringMap } from "@angular/compiler/src/compiler_facade_interface"
+
+//Criando a Classe Usuario -
+export class Usuario{
+    public id: number
+    public nome: string
+    public usuario: string
+    public senha: string
+    public foto: string
+    public tipo: string
+}
